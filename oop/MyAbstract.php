b6483eaa1a0a2b9fd86ecf5bd4acc582877c445d
ThuyHoaTruong <ht613687@gmail.com>
2024-09-24 08:22:27 +0700

    oop

---
+++ oop/MyAbstract.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class AbsA {
+    // Khai báo  định nghĩa hàm
+    public function Declare(){
+        echo 'abcd';
+    }
+    // Khai báo ko định nghĩa hàm
+    abstract function NotDeclare();
+    // Final trong abstract
+    
+     
+   
+}
+
+abstract class AbsB {
+    abstract function runB();
+    
+}
+
+abstract class AbsC {
+    abstract function runC();
+    
+}
+// sua 
+abstract class Abstract1 {
+    abstract public function func_from_Ab1_no_body();
+    
+}
+
---
+++ oop/MyInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+interface IA {
+  public function NotDeclare();
+
+}
+
+interface IB{
+   
+
+}
+interface IC{
+   
+
+}
+
+interface Interface1{
+  function func_from_Int1();
+
+}
+interface Interface2{
+  function func_from_Int2();
+
+}
+interface Interface3{
+  function func_from_Int3();
+
+}
+
+// interface Interface2 {
+
+// }
+
+// interface Interface3 {
+
+// }
